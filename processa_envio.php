8b61dc92a58399ca471dfa4ce304cdf1eade46e2
Jonathan Douglas Nunes de Souza <jonathansouza.nunes98@gmail.com>
2021-02-23 13:23:27 -0300

    feat: Adiciona Objeto Mensagem
    Foi criado um objeto mensagem responsável por criar atributos privados que representarão os campos do formulário de e-mail.
    Também foram criadas funções mágicas __get() e __set, p/ processar os dados recebidos dos campos 'assunto' e 'mensagem' de forma dinâmica.
    E por fim, foi criada uma função pública mensagemValida() que verifica se todos os campos foram preenchidos.

---
+++ processa_envio.php
@@ -0,0 +1,37 @@
+<?php 
+
+    // print_r($_POST);
+
+    class Mensagem {
+        private $destinatario = 'jonathannunes.souza98@gmail.com';
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo) {
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor) {
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida() {
+            if (empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+                return true;
+        }
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    if ($mensagem->mensagemValida()) {
+        echo 'Mensagem válida';
+    } else {
+        echo 'Mensagem inválida';
+    }
+
+?>
