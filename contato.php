2ddabd60bb2fa53522e9de9b234231125e6d2ac3
Jonathan Douglas Nunes de Souza <jonathansouza.nunes98@gmail.com>
2021-02-23 12:00:45 -0300

    fix: Insere link correto de referenciação ao bootstrap 4.1.3

--- contato.php
+++ contato.php
@@ -1,18 +1,22 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
   <head>
-    <meta charset="utf-8" />
-    <title>Finans | Contato </title>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <!-- Font Awesome -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 
-    <!-- Estilo CSS -->
+    <!-- Estilo -->
     <link rel="stylesheet" type="text/css" href="css/estilo.css">
-    
-  </head>
 
+    <title>Finans - Contato</title>
+
+  </head>
   <body>
     
   </body>
