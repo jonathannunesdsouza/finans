e73d8f1fd92872227f3fe3067314498a80ae737f
Jonathan Douglas Nunes de Souza <jonathansouza.nunes98@gmail.com>
2021-02-23 12:26:46 -0300

    style: Adiciona seção conteudo ná página contato.php
    Na página contato.php, foi adicionado uma seção com  a classe conteudo. Essa página possúi um formulário p/que o usuário
    entre em contato com a central de resolução de dúvidas do negócio (via e-mail).

--- contato.php
+++ contato.php
@@ -18,6 +18,67 @@
 
   </head>
   <body>
-    
+    <header><!-- inicio cabeçalho -->
+            
+        <nav class="navbar navbar-expand-sm navbar-light bg-warning">
+
+            <div class="container">
+
+                <a href="#" class="navbar-brand">
+                    <img src="imagens/logo.png" width="142">
+                </a>
+
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-principal">
+                    <span  class="navbar-toggler-icon"></span>
+                </button>
+                
+            </div><!-- menu de navegação -->
+            
+        </nav><!-- fim menu de navegação -->
+
+    </header><!-- fim cabeçalho -->
+    <section class="conteudo">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 d-flex">
+                    <div class="align-self-center">
+                        <h2>Entre em contato conosco e tire suas dúvidas</h2>
+
+                        <p class="lead">
+
+                            Para nós da Finans, é muito importante que você não tenha dúvidas
+                            sobre nossos serviços! Mande-nos um e-mail!
+                            
+                        </p>
+
+                        <img src="imagens/suporte-contato.png" width="426">
+                    </div>
+                </div>
+
+                <div class="col-md-6 d-flex">
+                    <div class="card-body font-weight-bold">
+                            <form>
+                                <div class="form-group">
+                                    <label for="para">Para</label>
+                                    <input type="text" class="form-control" id="para" placeholder="joao@dominio.com.br">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="assunto">Assunto</label>
+                                    <input type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="mensagem">Mensagem</label>
+                                    <textarea class="form-control" id="mensagem"></textarea>
+                                </div>
+
+                                <button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
+                            </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
   </body>
 </html>
