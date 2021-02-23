87436ee54cc24afb615759c321c0d070c13c5b23
Jonathan Douglas Nunes de Souza <jonathansouza.nunes98@gmail.com>
2021-02-23 10:46:08 -0300

    Adiciona arquivos iniciais ao projeto
    Foram adicionados os arquivos iniciais p/ desenvolvimento do projeto, tais como:
    index.php, contato.php, estilo.css e imagens.

---
+++ contato.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Finans | Contato </title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <!-- Estilo CSS -->
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    
+  </head>
+
+  <body>
+    
+  </body>
+</html>
